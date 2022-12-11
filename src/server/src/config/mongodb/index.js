cea65c14d6bcaf5a7694ab45eaa1bc8b8ea04f01
Andre <phandoananh1609@gmail.com>
2022-12-11 15:17:43 +0700

    MongoDB Cloud

--- src/server/src/config/mongodb/index.js
+++ src/server/src/config/mongodb/index.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 async function connect(){
     try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/HoB',{
+        await mongoose.connect('mongodb+srv://heavenofbook:admin123123@cluster0.s7xcxya.mongodb.net/test',{
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
